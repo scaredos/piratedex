4f9f57b591cf3fc4139bb392a685827a3919aa15
Steven <scared@tuta.io>
2023-04-08 13:57:36 -0400

    Fix for Linux (Ubuntu)
    `screen -ls` output returns different number of "\n" on macOS than Linux (Ubuntu)

--- api/screenWatcher.go
+++ api/screenWatcher.go
@@ -12,19 +12,20 @@ import (
 // The returned array contains the output of "screen -ls" as a list, seperated
 // by the newline character.
 func getScreens() []string {
-	cmd := exec.Command("/usr/bin/screen", "-ls")
+	cmd := exec.Command("screen", "-ls")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	var x []string
-	if err != nil {
+	if err == nil {
 		if strings.Contains(out.String(), "No sockets") {
 			x = nil
 		} else {
 			x = strings.Split(out.String(), "\n")
 		}
+		return x
 	}
-	return x
+	return nil
 }
 
 func main() {
@@ -32,22 +33,22 @@ func main() {
 
 	app.Get("/api/screens", func(c *fiber.Ctx) {
 		screens := getScreens()
-		if len(screens) == 3 {
+		if len(screens) == 0 {
 			c.JSON(&fiber.Map{
-				"success": true,
+				"success": false,
 				"screens": []string{},
 			})
 			return
+		} else {
+			screens = screens[:len(screens)-2]
+			screens = screens[1:]
+			c.JSON(&fiber.Map{
+				"success": true,
+				"screens": screens,
+			})
 		}
-		screens = screens[:len(screens)-3]
-		screens = screens[1:]
-		c.JSON(&fiber.Map{
-			"success": true,
-			"screens": screens,
-		})
 		return
 	})
-  
-  // Port 2052 so I am able to route these through Cloudflare and not expose server IPs
+
 	app.Listen(2052)
 }
