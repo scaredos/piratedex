0617e889d35ca335b204a23907371918daae39af
Steven <scared@tuta.io>
2023-07-03 11:25:53 -0400

    Update magnetextract.py

--- utils/magnetextract.py
+++ utils/magnetextract.py
@@ -1,19 +1,16 @@
 from bs4 import BeautifulSoup
 
 def find_magnets(html: str):
-    """Find magnet URL from html
+    """Find magnet URLs from HTML
 
     :param html: HTML from website containing magnet links
-
-    :returns: Generator of magnets from HTML provided
+    :returns: Generator of magnet URLs from the provided HTML
     """
 
-    # Create soup interpreter
-    soup = BeautifulSoup(html)
+    # Create soup interpreter with explicit parser specified
+    soup = BeautifulSoup(html, 'html.parser')
 
     for a in soup.find_all('a', href=True):
         magnet_link = a['href']
-        if 'magnet:?' in magnet_link:
-            # We want to reject releases with no names
-            if 'dn=' in magnet_link:
-                yield magnet_link
+        if 'magnet:?' in magnet_link and 'dn=' in magnet_link:
+            yield magnet_link
