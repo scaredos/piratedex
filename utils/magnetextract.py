95b19f8b7c5e9cb58c0d527c3211c888b2a47cc7
Steven <scared@tuta.io>
2023-07-03 11:24:51 -0400

    Update magnetextract.py

--- utils/magnetextract.py
+++ utils/magnetextract.py
@@ -1,25 +1,19 @@
 from bs4 import BeautifulSoup
 
-class MagnetExtract:
-  def __init__(self, html: str):
-    """
-    Magnet Extraction module that returns list of magnets from HTML of website.
-    Only works if the magnet URLs are hyperlinked
-    
-    Parameters:
-    html (str): HTML file as string
-    
-    Returns:
-    list: List of magnets
-    """
-    # Create soup interpreter
-    soup = BeautifulSoup(html, features="lxml")
-    magnets = []
-    for a in soup.find_all('a', href=True):
-        link = a['href']
-        if 'magnet:?' in link:
-            if link not in magnets:
-                magnets.append(link)
+def find_magnets(html: str):
+    """Find magnet URL from html
 
-    self.magnets = magnets
-  
+    :param html: HTML from website containing magnet links
+
+    :returns: Generator of magnets from HTML provided
+    """
+
+    # Create soup interpreter
+    soup = BeautifulSoup(html)
+
+    for a in soup.find_all('a', href=True):
+        magnet_link = a['href']
+        if 'magnet:?' in magnet_link:
+            # We want to reject releases with no names
+            if 'dn=' in magnet_link:
+                yield magnet_link
